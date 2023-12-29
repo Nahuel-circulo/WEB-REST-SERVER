52156ba2d80b945f651270df96cfe964964e95bd
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-12-29 15:32:06 -0300

    config response

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -8,16 +8,22 @@ export class Server {
   private port = 3000;
 
 
-  
-  
-  
+
+
+
   async start() {
 
 
     // Middlewares
-  
+
     // Public Folder
     this.app.use(express.static('public'));
+
+
+    this.app.get('*', (req, res) => {
+      console.log(req.url)
+      res.send('Hola Mundo')
+    })
 
 
     this.app.listen(this.port, () => {
