f5070933b1e7fb983c8c61b70b1cc871922e7d41
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-12-29 16:16:48 -0300

    global routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,33 +1,12 @@
 import { Router } from "express";
+import { TodoRoutes } from "./todos/routes";
 
 
 export class AppRoutes {
 
   static get routes(): Router {
-
     const router = Router();
-
-    router.get('/api/todos', (req, res) => {
-      res.json([
-        {
-          id: 1,
-          text: 'Learn Docker',
-          createdAt: new Date()
-        },
-        {
-          id: 2,
-          text: 'Learn CI/CD',
-          createdAt: new Date()
-        },
-        {
-          id: 3,
-          text: 'Learn SVG Animations',
-          createdAt: new Date()
-        },
-      ])
-    })
-
-
+    router.use('/api/todos', TodoRoutes.routes)
     return router;
   }
 
