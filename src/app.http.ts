80f67ce0ff002a6c86d7881f2451f7fb9cc77e0a
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-12-29 14:21:54 -0300

    practice http2

--- src/app.http.ts
+++ src/app.http.ts
@@ -1,10 +1,7 @@
-import http2 from 'http2'
+import http from 'http'
 import fs from 'fs'
 
-const server = http2.createSecureServer({
-  key: fs.readFileSync('./keys/server.key'),
-  cert: fs.readFileSync('./keys/server.crt')
-}, (req, res) => {
+const server = http.createServer((req, res) => {
 
 
   if (req.url === '/') {
@@ -20,14 +17,9 @@ const server = http2.createSecureServer({
   else if (req.url?.endsWith('.css')) {
     res.writeHead(200, { 'Content-Type': 'text/css' })
   }
-try{
 
   const responseContent = fs.readFileSync(`./public/${req.url}`, 'utf-8');
   res.end(responseContent)
-}catch(e){
-  res.writeHead(404, { 'Content-Type': 'text/html' })
-  res.end('<h1>Page not found</h1>')
-}
 
 
 })
