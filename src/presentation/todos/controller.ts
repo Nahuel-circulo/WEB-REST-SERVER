4a30886a92757a4040f1e7678f1450f238b64dbd
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-02 15:40:21 -0300

    update todo on database

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -63,22 +63,31 @@ export class TodosController {
     res.json(todo);
   }
 
-  public updateTodo = (req: Request, res: Response) => {
+  public updateTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
     if (isNaN(id)) return res.status(400).json({ error: 'ID argument is not a number' });
 
-    const todo = todos.find(todo => todo.id === id);
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id
+      }
+    })
+
     if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found` });
 
     const { text, completedAt } = req.body;
 
-    todo.text = text || todo.text;
-    (completedAt === 'null')
-      ? todo.completedAt = null
-      : todo.completedAt = new Date(completedAt || todo.completedAt);
+    const updatedTodo = await prisma.todo.update({
+      where: {
+        id
+      },
+      data: {
+        completedAt,
+        text
+      }
+    })
 
-
-    res.json(todo);
+    res.json(updatedTodo);
 
   }
 
