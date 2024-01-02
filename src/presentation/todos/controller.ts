b3d1caeed18ea2afd302af07770551fbc1dd27a1
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-02 13:37:29 -0300

    delete todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -76,4 +76,15 @@ export class TodosController {
 
   }
 
+  public deleteTodo = (req:Request, res: Response) => {
+    const id = +req.params.id;
+
+    const todo = todos.find(todo => todo.id === id );
+    if ( !todo ) return res.status(404).json({ error: `Todo with id ${ id } not found` });
+
+    todos.splice( todos.indexOf(todo), 1 );
+    res.json( todo );
+
+  }
+
 }
