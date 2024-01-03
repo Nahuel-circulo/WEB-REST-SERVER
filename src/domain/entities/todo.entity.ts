cbc181dc968e0a551eddf7b224a35e3b363b7af5
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-03 16:11:17 -0300

    implenet TodoRepository and datasource

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -12,7 +12,7 @@ export class TodoEntity {
     return !!this.completedAt;
   }
 
-  public static fromObject(object: { [key: string]: any }) {
+  public static fromObject(object: { [key: string]: any }): TodoEntity {
     const { id, text, completedAt } = object;
     if (!id || !text) {
       throw new Error('Id and text are required');
---
+++ src/infrastructure/repository/todo.respository.impl.ts
@@ -0,0 +1,24 @@
+import { CreateTodoDto, TodoDatasource, TodoEntity, TodoRepository, UpdateTodoDto } from "../../domain";
+
+
+export class TodoRespositoryImpl implements TodoRepository{
+  constructor(
+    private readonly datasource:TodoDatasource
+  ){}
+  create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+    return this.datasource.create(createTodoDto)
+  }
+  getAll(): Promise<TodoEntity[]> {
+    return this.datasource.getAll()
+  }
+  getById(id: number): Promise<TodoEntity> {
+    return this.datasource.getById(id)
+  }
+  updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+    return this.datasource.updateById( updateTodoDto)
+  }
+  deleteById(id: number): Promise<TodoEntity> {
+    return this.datasource.deleteById(id)
+  }
+
+}
