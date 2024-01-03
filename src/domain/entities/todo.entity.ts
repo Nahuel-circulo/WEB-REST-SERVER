f44a28681a4b799fbbf8a4cbed921d8b39c2760e
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-01-03 15:53:22 -0300

    create todo from object

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -12,4 +12,20 @@ export class TodoEntity {
     return !!this.completedAt;
   }
 
+  public static fromObject(object: { [key: string]: any }) {
+    const { id, text, completedAt } = object;
+    if (!id || !text) {
+      throw new Error('Id and text are required');
+    }
+
+    let newCompletedAt;
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) {
+        throw new Error('CompletedAt is not a valid date');
+      }
+    }
+    return new TodoEntity(id, text, completedAt);
+  }
+
 }
